08259ef99bf326f18d2223e50f03d39928de404b
manisero <michalaniserowicz@gmail.com>
2017-04-25 19:49:06 +0200

    Converted Root to functional component

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 import TextBox from './TextBox.jsx'
 import Info from './Info.jsx'
 
-class Root extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div>
-				<TextBox  {...this.props} />
-				<Info {...this.props} />
-			</div>
-		);
-	}
-}
+var Root = function(props) {
+	return (
+		<div>
+			<TextBox  {...props} />
+			<Info {...props} />
+		</div>
+	);
+};
 
 export default Root;
