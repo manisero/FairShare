328505e119cb6dca32c1989d192ce8e675d990b8
manisero <michalaniserowicz@gmail.com>
2017-04-25 21:59:50 +0200

    Refactoring to better express modularity

--- app/store.js
+++ app/store.js
@@ -1,48 +1,50 @@
 import { createStore } from 'redux'
 
+// dev tools
+var reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// events
+var INPUT_CHANGED = 'INPUT_CHANGED';
+var INPUT_LENGTH_CHANGED = 'INPUT_LENGTH_CHANGED';
+
+// initial state
+var initialInputValue = 'test';
+
+var state = {
+	input: {
+		value: initialInputValue,
+		length: initialInputValue.length
+	},
+	info: {
+		inputLength: initialInputValue.length
+	}
+};
+
+// reducer
+var reducer = (state, action) => {
+	switch (action.type) {
+	case INPUT_CHANGED:
+		return {
+			input: {
+				value: action.value,
+				length: action.value.length
+			},
+			info: state.info
+		};
+	case INPUT_LENGTH_CHANGED:
+		return {
+			input: state.input,
+			info: {
+				inputLength: action.length
+			}
+		};
+	default:
+		return state;
+	}
+};
+
+// initStore
 var initStore = function() {
-	// events
-	var INPUT_CHANGED = 'INPUT_CHANGED';
-	var INPUT_LENGTH_CHANGED = 'INPUT_LENGTH_CHANGED';
-
-	// initial state
-	var initialInputValue = 'test';
-
-	var state = {
-		input: {
-			value: initialInputValue,
-			length: initialInputValue.length
-		},
-		info: {
-			inputLength: initialInputValue.length
-		}
-	};
-
-	// reducer
-	var reducer = (state, action) => {
-		switch (action.type) {
-		case INPUT_CHANGED:
-			return {
-				input: {
-					value: action.value,
-					length: action.value.length
-				},
-				info: state.info
-			};
-		case INPUT_LENGTH_CHANGED:
-			return {
-				input: state.input,
-				info: {
-					inputLength: action.length
-				}
-			};
-		default:
-			return state;
-		}
-	};
-
-	// store
-	var reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 	var store = createStore(reducer, state, reduxDevTools);
 
 	return {
