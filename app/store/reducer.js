08fbc5d6c95c25db25be3b5784dd2f96b8310706
manisero <michalaniserowicz@gmail.com>
2017-05-16 22:28:28 +0200

    Used combineReducers

--- app/store/reducer.js
+++ app/store/reducer.js
@@ -1,29 +1,25 @@
+import { combineReducers } from "redux";
 import { actions } from './actions'
 
-let onChangeInput = (state, action) => ({
-    input: {
-        value: action.value,
-        length: action.value.length
-    },
-    info: state.info
-});
-
-let onChangeInputLength = (state, action) => ({
-    input: state.input,
-    info: {
-        inputLength: action.length
+let input = (state = { value: '', length: 0 }, action) => {
+    if (action.type === actions.CHANGE_INPUT) {
+        return {
+            value: action.value,
+            length: action.value.length
+        };
+    } else {
+        return state;
     }
-});
-
-let reducer = (state, action) => {
-	switch (action.type) {
-	case actions.CHANGE_INPUT:
-		return onChangeInput(state, action);
-	case actions.CHANGE_INPUT_LENGTH:
-		return onChangeInputLength(state, action);
-	default:
-		return state;
-	}
 };
 
-export default reducer;
+let info = (state = { inputLength: 0 }, action) => {
+    if (action.type === actions.CHANGE_INPUT_LENGTH) {
+        return {
+            inputLength: action.length
+        };
+    } else {
+        return state;
+    }
+};
+
+export default combineReducers({ input, info });
