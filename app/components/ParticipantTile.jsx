6170d18b3e2b541b78618c53b3616d76d9ea2032
manisero <michalaniserowicz@gmail.com>
2017-06-01 22:32:14 +0200

    Participant selection is now visible

--- app/components/ParticipantTile.jsx
+++ app/components/ParticipantTile.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { connect } from 'reactReduxUtils'
 
-let ParticipantTile = ({ participant, onClick }) => (
+let defaultNameStyle = {};
+
+let selectedNameStyle = {
+	fontWeight: 'bold'
+};
+
+let ParticipantTile = ({ participant, isSelected, onClick }) => (
 	<div onClick={() => onClick()}>
-		<div>Name: {participant.name}</div>
+		<div>Name: <span style={isSelected ? selectedNameStyle : defaultNameStyle}>{participant.name}</span></div>
 		<div>Contribution: {participant.contribution}</div>
 	</div>
 );
 
 let mapStateToProps = (state, { participantId }) => ({
-	participant: state.data.participants.items[participantId]
+	participant: state.data.participants.items[participantId],
+	isSelected: participantId === state.ui.selectedParticipantId 
 });
 
 let mapEventsToProps = (events, { participantId }) => ({
