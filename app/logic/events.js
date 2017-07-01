8cefcb36682c08e592fbf018e86cc2ba370c9a48
manisero <michalaniserowicz@gmail.com>
2017-07-01 13:26:55 +0200

    Started making events generic

--- app/logic/events.js
+++ app/logic/events.js
@@ -2,6 +2,16 @@ import update from 'immutability-helper'
 import { EntityType } from 'model'
 
 let eventDataCreators = ({
+    entitySelected: (entity, id) => ({ entity,id }),
+	entityDeselected: entity => ({ entity }),
+	entityAdded: entity => ({ entity }),
+	entityEdit_Started: (entity, id) => ({ entity, id }),
+	entityEdit_Updated: (entity, id, updateCommand) => ({ entity, id, updateCommand }),
+	entityEdit_Submitted: (entity, id) => ({ entity, id }),
+	entityEdit_Cancelled: (entity, id) => ({ entity, id }),
+	entityDelete_Started: (entity, id) => ({ entity, id }),
+	entityDelete_Submitted: (entity, id) => ({ entity, id }),
+	entityDelete_Cancelled: (entity, id) => ({ entity, id }),
 	// Participant:
     participantSelected: participantId => ({ participantId }),
 	participantDeselected: () => ({}),
@@ -27,6 +37,57 @@ let eventDataCreators = ({
 });
 
 let subscribe = (events, store) => {
+	events.entitySelected.stream
+		.subscribe(e => store.actions.selectEntity(e.data.entity, e.data.id, e));
+	
+	events.entityDeselected.stream
+		.subscribe(e => store.actions.deselectEntity(e.data.entity, e));
+
+	events.entityAdded.stream
+		.subscribe(e => {
+			let entity = e.data.entity;
+			let id = store.getState().data[entity].lastId + 1;
+			let item = { // TODO: Make generic
+				name: '',
+				contribution: 0
+			};
+
+			store.actions.addEntity(entity, id, item, e);
+			store.actions.editEntity_start(entity, id, e);
+		});
+	
+	events.entityEdit_Started.stream
+		.subscribe(e => store.actions.editEntity_start(e.data.entity, e.data.id, e));
+	
+	events.entityEdit_Updated.stream
+		.subscribe(e => {
+			let entity = e.data.entity;
+			let id = e.data.id;
+			let item = store.getState().ui[entity].edit[id];
+			let newItem = update(item, e.data.updateCommand);
+			let validationError = validateParticipant(newItem); // TODO: Make generic
+
+			store.actions.editEntity_update(entity, id, newItem, e);
+
+			if (validationError != null) {
+				console.log(validationError);
+			}
+		});
+	
+	events.entityEdit_Submitted.stream
+		.subscribe(e => store.actions.editEntity_submitFocused(e.data.entity, e));
+
+	events.entityEdit_Cancelled.stream
+		.subscribe(e => store.actions.editEntity_cancelFocused(e.data.entity, e));
+	
+	events.entityDelete_Started.stream
+		.subscribe(e => store.actions.deleteEntity_start(e.data.entity, e.data.id, e));
+	
+	events.entityDelete_Submitted.stream
+		.subscribe(e => store.actions.deleteEntity_submitFocused(e.data.entity, e));
+
+	events.entityDelete_Cancelled.stream
+		.subscribe(e => store.actions.deleteEntity_cancelFocused(e.data.entity, e));
 
 	// Participant:
 	events.participantSelected.stream
