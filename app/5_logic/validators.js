068463330993995e67e1152d298b80be76c3e6fe
manisero <michalaniserowicz@gmail.com>
2017-07-02 18:34:37 +0200

    Implemented Item validation

--- app/5_logic/validators.js
+++ app/5_logic/validators.js
@@ -2,7 +2,7 @@ let validateParticipant = (participant, state) => {
 	let invalid = false;
 	let error = {};
 
-	if (participant.name == null || participant.name == 0) {
+	if (participant.name == null || participant.name == '') {
 		invalid = true;
 		error.name = 'Name cannot be empty.'
 	}
@@ -15,6 +15,24 @@ let validateParticipant = (participant, state) => {
 	return invalid ? error : null;
 };
 
+let validateItem = (item, state) => {
+	let invalid = false;
+	let error = {};
+
+	if (item.name == null || item.name == '') {
+		invalid = true;
+		error.name = 'Name cannot be empty.'
+	}
+
+	if (item.price != null && item.price < 0) {
+		invalid = true;
+		error.price = 'Price cannot be negative.'
+	}
+
+	return invalid ? error : null;
+};
+
 export default {
-	participant: validateParticipant
+	participant: validateParticipant,
+	item: validateItem
 };
