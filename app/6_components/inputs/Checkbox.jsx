03cda5df3cefaae9fbd36f4613833d78db4b9191
manisero <michalaniserowicz@gmail.com>
2017-07-03 23:40:23 +0200

    Checkbox cleanup

--- app/6_components/inputs/Checkbox.jsx
+++ app/6_components/inputs/Checkbox.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 let getNewValue = e => e.target.checked;
 
 let Checkbox = props => (
-	<div className="checkbox">
+	<div className='checkbox'>
 		<label>
-			<input type="checkbox" {...props} onChange={e => props.onChange(getNewValue(e))} /> {props.label}
+			<input type='checkbox' checked={props.checked} onChange={e => props.onChange(getNewValue(e))} /> {props.label}
 		</label>
 	</div>
 );
