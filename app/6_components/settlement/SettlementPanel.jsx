dc293c9ca2cbbd6a3703e2931d99421508cfed0c
manisero <michalaniserowicz@gmail.com>
2017-07-08 19:42:02 +0200

    Displayed payments in SettlementPanel

--- app/6_components/settlement/SettlementPanel.jsx
+++ app/6_components/settlement/SettlementPanel.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import { connect } from 'reactReduxUtils'
+import { EntityType } from 'model'
+import queries from 'queries'
 import { Button } from 'inputs'
 
-let SettlementPanel = ({ title, onSettleClick }) => {
+let SettlementPanel = ({ paymentIds, payments, participants, onSettleClick }) => {
+    let paymentElements = paymentIds.map(paymentId => {
+        let payment = payments[paymentId];
+        let payer = participants[payment.payerId];
+        let payee = participants[payment.payeeId];
+
+        return (
+            <div key={paymentId}>
+                {payer.name} owes {payment.amount} to {payee.name}
+            </div>
+        );
+    });
+
     return (
         <div className='panel panel-default'>
             <div className='panel-heading'>
-                <h3 className='panel-title'>{title}</h3>
+                <h3 className='panel-title'>Settlement</h3>
             </div>
             <div className='panel-body'>
                 <Button onClick={() => onSettleClick()}>Settle</Button>
+            </div>
+            <div>
+                {paymentElements}
             </div>
         </div>
     );
 };
 
 let mapStateToProps = state => ({
-    title: 'Settlement'
+    paymentIds: queries.entityIds(state, EntityType.payment),
+    payments: queries.entityAllData(state, EntityType.payment),
+    participants: queries.entityAllData(state, EntityType.participant)
 });
 
 let mapEventsToProps = events => ({
