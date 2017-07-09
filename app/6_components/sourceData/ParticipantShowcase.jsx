c20002aa59e81007330697190c921bfb5e29b7c6
manisero <michalaniserowicz@gmail.com>
2017-07-09 11:13:00 +0200

    Removed contribution from ParticipantShowcase

--- app/6_components/sourceData/ParticipantShowcase.jsx
+++ app/6_components/sourceData/ParticipantShowcase.jsx
@@ -2,12 +2,10 @@ import React from 'react'
 import { connect } from 'reactReduxUtils'
 import { EntityType } from 'model'
 import queries from 'queries'
-import { Money } from 'compUtils'
 
 let ParticipantShowcase = ({ participant }) => (
 	<div>
-		<div>Name: {participant.name}</div>
-		<div>Contribution: <Money amount={participant.contribution} /></div>
+		<div>{participant.name}</div>
 	</div>
 );
 
