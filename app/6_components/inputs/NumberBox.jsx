1f475b48da1b5599178e5f8fbfa3eb785544dff8
manisero <michalaniserowicz@gmail.com>
2017-07-09 19:57:16 +0200

    NumberBox now handles comma as decimal separator

--- app/6_components/inputs/NumberBox.jsx
+++ app/6_components/inputs/NumberBox.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import TextBox from './TextBox.jsx'
 
 const DecimalSeparator = '.';
+const NotSupportedDecimalSeparator = ',';
 
 let parseAndCheckImplicitDecimal = (valueString) => {
+	valueString = valueString.replace(NotSupportedDecimalSeparator, DecimalSeparator);
+
 	let value = parseFloat(valueString);
-	value = !isNaN(value) ? value : 0
+	value = !isNaN(value) ? value : 0;
 
 	let isExplicitDecimal = value % 1 !== 0;
 	let isImplicitDecimal = !isExplicitDecimal && valueString.endsWith(DecimalSeparator);
