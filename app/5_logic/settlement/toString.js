84e0d1aa60f2724ce2306b7f9a1b34da34fbdad1
manisero <michalaniserowicz@gmail.com>
2017-07-11 23:42:37 +0200

    Implemented formatting settlement to string

--- app/5_logic/settlement/toString.js
+++ app/5_logic/settlement/toString.js
@@ -2,15 +2,17 @@ let formatParticipant = participant => participant != null
     ? participant.name
     : '[Unknown]';
 
+let formatAmount = amount => amount.toFixed(2);
+
 let formatPayment = (payment, payee) =>
-    '- ' + payment.amount + ' to ' + formatParticipant(payee);
+    '- ' + formatAmount(payment.amount) + ' to ' + formatParticipant(payee);
 
 let formatPayerPayments = (payerId, payments, participants) => {
     let payerLine = formatParticipant(participants[payerId]) + ' owes:';
 
     let paymentLines = payments.map(p => formatPayment(p, participants[p.payeeId]));
 
-    return [payerLine, ...paymentLines];
+    return [payerLine, ...paymentLines].join('\n');
 };
 
 let toString = (paymentsByPayerId, participants) => {
